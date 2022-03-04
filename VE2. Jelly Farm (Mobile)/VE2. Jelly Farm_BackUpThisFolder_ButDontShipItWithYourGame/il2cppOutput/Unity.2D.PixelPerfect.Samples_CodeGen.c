80314e17534734a5419b77b9a4078a190e21a047
MelonMinion <dnjsdn1217@naver.com>
2022-03-04 15:57:54 +0900

    Completed Project
    The result of the project using Unity.

---
+++ VE2. Jelly Farm (Mobile)/VE2. Jelly Farm_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Unity.2D.PixelPerfect.Samples_CodeGen.c
@@ -0,0 +1,157 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CustomCanvasScaler::OnEnable()
+extern void CustomCanvasScaler_OnEnable_mB6C98D414B10A8BB2005F7E10A1E9971A7FBF7E9 (void);
+// 0x00000002 System.Void CustomCanvasScaler::HandleScaleWithScreenSize()
+extern void CustomCanvasScaler_HandleScaleWithScreenSize_mEF0FDFEFB14CED94E8A5951E0642E96D08EC1878 (void);
+// 0x00000003 System.Void CustomCanvasScaler::.ctor()
+extern void CustomCanvasScaler__ctor_m3FD4635A82FD5949078743157B1537DF2295B1EB (void);
+// 0x00000004 System.Void Hero::Start()
+extern void Hero_Start_m54FCA87FBB8D2380C1FCCD82A8E00B253215C824 (void);
+// 0x00000005 System.Void Hero::Update()
+extern void Hero_Update_mCDD0AC705ACAD253F8FFB1CC4C386256EE5A8459 (void);
+// 0x00000006 System.Void Hero::LevelComplete()
+extern void Hero_LevelComplete_mD5B8CA9A3684C1B01635D5DFB4F458817A2CBD0C (void);
+// 0x00000007 System.Void Hero::.ctor()
+extern void Hero__ctor_mC6CDC89390E1FB717C118D299CD3A42DF5583122 (void);
+// 0x00000008 System.Void BasicCameraFollow::Update()
+extern void BasicCameraFollow_Update_m38DA6E1013BC9344A3F9C3BB1B36D315BD270387 (void);
+// 0x00000009 System.Void BasicCameraFollow::.ctor()
+extern void BasicCameraFollow__ctor_m6180DBE70EAC7251FDBD27CD397484F11B487624 (void);
+// 0x0000000A System.Void CameraManager::Awake()
+extern void CameraManager_Awake_m02A46CC0FBBB43B1216F90D62044A178899D3D84 (void);
+// 0x0000000B System.Void CameraManager::TogglePixelPerfect(System.Boolean)
+extern void CameraManager_TogglePixelPerfect_m7FD7E51D34A7CBB5A0B209EBA992EF017757425F (void);
+// 0x0000000C System.Void CameraManager::ValidateCameras(System.Boolean)
+extern void CameraManager_ValidateCameras_m9847CEACD5752BF2A99649A9DCC543B5412946FE (void);
+// 0x0000000D System.Void CameraManager::.ctor()
+extern void CameraManager__ctor_m7D095B25DCA00C737AFBE8EBE4FEBFF808FC839C (void);
+// 0x0000000E System.Void HeroMover::Start()
+extern void HeroMover_Start_m9550476943452051C77AB76A209FCAA31AD57BF1 (void);
+// 0x0000000F System.Void HeroMover::Update()
+extern void HeroMover_Update_mE3CF239EB4861158BE7B4A3C960A953E88BE70F8 (void);
+// 0x00000010 System.Void HeroMover::.ctor()
+extern void HeroMover__ctor_mB8838105915DF850D31370681E8334F7995C1579 (void);
+// 0x00000011 System.Void UnityEngine.RuleTile::GetTileData(UnityEngine.Vector3Int,UnityEngine.Tilemaps.ITilemap,UnityEngine.Tilemaps.TileData&)
+extern void RuleTile_GetTileData_mC8EFF56BBE17123852FB1313284A179956DC98B9 (void);
+// 0x00000012 System.Single UnityEngine.RuleTile::GetPerlinValue(UnityEngine.Vector3Int,System.Single,System.Single)
+extern void RuleTile_GetPerlinValue_m4C046DD42C7BF9737DA66A9AE8324E02978EAB1C (void);
+// 0x00000013 System.Boolean UnityEngine.RuleTile::GetTileAnimationData(UnityEngine.Vector3Int,UnityEngine.Tilemaps.ITilemap,UnityEngine.Tilemaps.TileAnimationData&)
+extern void RuleTile_GetTileAnimationData_mDCDC82CDAF8375572FD604429156CD2148FD99E5 (void);
+// 0x00000014 System.Void UnityEngine.RuleTile::RefreshTile(UnityEngine.Vector3Int,UnityEngine.Tilemaps.ITilemap)
+extern void RuleTile_RefreshTile_m63E21D4E1CA300CFF1663A047E8F9E28A1968F1D (void);
+// 0x00000015 System.Boolean UnityEngine.RuleTile::RuleMatches(UnityEngine.RuleTile/TilingRule,UnityEngine.Vector3Int,UnityEngine.Tilemaps.ITilemap,UnityEngine.Matrix4x4&)
+extern void RuleTile_RuleMatches_m3E71FC312965E255C3555D640610BC99DE879E8B (void);
+// 0x00000016 UnityEngine.Matrix4x4 UnityEngine.RuleTile::ApplyRandomTransform(UnityEngine.RuleTile/TilingRule/Transform,UnityEngine.Matrix4x4,System.Single,UnityEngine.Vector3Int)
+extern void RuleTile_ApplyRandomTransform_m8D752FB204701C8F61618EB8E57CC46DE77818C3 (void);
+// 0x00000017 System.Boolean UnityEngine.RuleTile::RuleMatches(UnityEngine.RuleTile/TilingRule,UnityEngine.Vector3Int,UnityEngine.Tilemaps.ITilemap,System.Int32)
+extern void RuleTile_RuleMatches_m23F063F6544EAA5BC9494A9B3D93ACB7658D66D9 (void);
+// 0x00000018 System.Boolean UnityEngine.RuleTile::RuleMatches(UnityEngine.RuleTile/TilingRule,UnityEngine.Vector3Int,UnityEngine.Tilemaps.ITilemap,System.Boolean,System.Boolean)
+extern void RuleTile_RuleMatches_mA621C756E65B0168D9C9583D0D348614133E1F59 (void);
+// 0x00000019 System.Int32 UnityEngine.RuleTile::GetIndexOfOffset(UnityEngine.Vector3Int)
+extern void RuleTile_GetIndexOfOffset_m36FB23ECFD507359D42F2D2AE03896930EB50C69 (void);
+// 0x0000001A UnityEngine.Vector3Int UnityEngine.RuleTile::GetRotatedPos(UnityEngine.Vector3Int,System.Int32)
+extern void RuleTile_GetRotatedPos_mDAF2BF54583500406C2526B197424CF927AE27E3 (void);
+// 0x0000001B UnityEngine.Vector3Int UnityEngine.RuleTile::GetMirroredPos(UnityEngine.Vector3Int,System.Boolean,System.Boolean)
+extern void RuleTile_GetMirroredPos_mEA0235713C7D465A9AEE35696459238DA15257A6 (void);
+// 0x0000001C System.Void UnityEngine.RuleTile::.ctor()
+extern void RuleTile__ctor_mBC6D3F2596C4EEDC487FD29EECC91E92D96BBE6B (void);
+// 0x0000001D System.Void UnityEngine.RuleTile/TilingRule::.ctor()
+extern void TilingRule__ctor_mA17ACE7060667AD3C36C42FA0DFA5BCF2092D4AD (void);
+static Il2CppMethodPointer s_methodPointers[29] = 
+{
+	CustomCanvasScaler_OnEnable_mB6C98D414B10A8BB2005F7E10A1E9971A7FBF7E9,
+	CustomCanvasScaler_HandleScaleWithScreenSize_mEF0FDFEFB14CED94E8A5951E0642E96D08EC1878,
+	CustomCanvasScaler__ctor_m3FD4635A82FD5949078743157B1537DF2295B1EB,
+	Hero_Start_m54FCA87FBB8D2380C1FCCD82A8E00B253215C824,
+	Hero_Update_mCDD0AC705ACAD253F8FFB1CC4C386256EE5A8459,
+	Hero_LevelComplete_mD5B8CA9A3684C1B01635D5DFB4F458817A2CBD0C,
+	Hero__ctor_mC6CDC89390E1FB717C118D299CD3A42DF5583122,
+	BasicCameraFollow_Update_m38DA6E1013BC9344A3F9C3BB1B36D315BD270387,
+	BasicCameraFollow__ctor_m6180DBE70EAC7251FDBD27CD397484F11B487624,
+	CameraManager_Awake_m02A46CC0FBBB43B1216F90D62044A178899D3D84,
+	CameraManager_TogglePixelPerfect_m7FD7E51D34A7CBB5A0B209EBA992EF017757425F,
+	CameraManager_ValidateCameras_m9847CEACD5752BF2A99649A9DCC543B5412946FE,
+	CameraManager__ctor_m7D095B25DCA00C737AFBE8EBE4FEBFF808FC839C,
+	HeroMover_Start_m9550476943452051C77AB76A209FCAA31AD57BF1,
+	HeroMover_Update_mE3CF239EB4861158BE7B4A3C960A953E88BE70F8,
+	HeroMover__ctor_mB8838105915DF850D31370681E8334F7995C1579,
+	RuleTile_GetTileData_mC8EFF56BBE17123852FB1313284A179956DC98B9,
+	RuleTile_GetPerlinValue_m4C046DD42C7BF9737DA66A9AE8324E02978EAB1C,
+	RuleTile_GetTileAnimationData_mDCDC82CDAF8375572FD604429156CD2148FD99E5,
+	RuleTile_RefreshTile_m63E21D4E1CA300CFF1663A047E8F9E28A1968F1D,
+	RuleTile_RuleMatches_m3E71FC312965E255C3555D640610BC99DE879E8B,
+	RuleTile_ApplyRandomTransform_m8D752FB204701C8F61618EB8E57CC46DE77818C3,
+	RuleTile_RuleMatches_m23F063F6544EAA5BC9494A9B3D93ACB7658D66D9,
+	RuleTile_RuleMatches_mA621C756E65B0168D9C9583D0D348614133E1F59,
+	RuleTile_GetIndexOfOffset_m36FB23ECFD507359D42F2D2AE03896930EB50C69,
+	RuleTile_GetRotatedPos_mDAF2BF54583500406C2526B197424CF927AE27E3,
+	RuleTile_GetMirroredPos_mEA0235713C7D465A9AEE35696459238DA15257A6,
+	RuleTile__ctor_mBC6D3F2596C4EEDC487FD29EECC91E92D96BBE6B,
+	TilingRule__ctor_mA17ACE7060667AD3C36C42FA0DFA5BCF2092D4AD,
+};
+static const int32_t s_InvokerIndices[29] = 
+{
+	4185,
+	4185,
+	4185,
+	4185,
+	4185,
+	4185,
+	4185,
+	4185,
+	4185,
+	4185,
+	3384,
+	3384,
+	4185,
+	4185,
+	4185,
+	4185,
+	997,
+	5317,
+	829,
+	1911,
+	562,
+	4848,
+	563,
+	303,
+	2431,
+	1502,
+	853,
+	4185,
+	4185,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Unity_2D_PixelPerfect_Samples_CodeGenModule;
+const Il2CppCodeGenModule g_Unity_2D_PixelPerfect_Samples_CodeGenModule = 
+{
+	"Unity.2D.PixelPerfect.Samples.dll",
+	29,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
